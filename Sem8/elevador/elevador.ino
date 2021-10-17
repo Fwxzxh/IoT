07a02a7a8e1087bb05c2353fa9c3ccbc33330fc4
Fwxzxh <jorgedavidmejia@hotmail.com>
2021-10-17 18:13:24 -0500

    Finished late programs lol im sorry

--- Sem8/elevador/elevador.ino
+++ Sem8/elevador/elevador.ino
@@ -1,6 +1,86 @@
+// Elevador.
+// Dos niveles.
+// El elevador se bloquea cuando esta en movimiento o esperando gente.
+// x f F
+// f 1 2
+// w 1 2
+// W 2 1
+
+#define MotorUp 13
+#define MotorDown 14
+#define Button1 15 // Se utiliza para llamar al elevador en la planta 1
+#define Button2 16 // Llamar al elevador en la planta 2
+#define Doors 17 // Puertas del elevador OFF/close ON/open
+
+enum Estados{
+    Floor,
+    Working,
+    Wait,
+};
+
+enum Plantas{
+    Planta1,
+    Planta2
+};
+
+Estados listaEstados[3] = {Floor, Wait, Working};
+
+Estados estadoActual;
+Estados estadoAnterior;
 
 void setup() {
+    Serial.begin(9600);
+    pinMode(13, OUTPUT);
+    pinMode(14, OUTPUT);
+    pinMode(15, INPUT);
+    pinMode(16, INPUT);
+    pinMode(17, OUTPUT);
+}
+
+void states(Estados estado) {
+    switch (estado) {
+        case Floor:
+            Serial.println("Elevador en Planta ");
+            delay(500);
+            Serial.println("Abriendo puertas...");
+            digitalWrite(17, HIGH);
+            Serial.println("Puertas abiertas.");
+            break;
+
+        case Working:
+            Serial.println("Cerrando Puertas.");
+            delay(1000);
+            Serial.println("Elevador trabajando");
+            delay(1000);
+            break;
+
+        case Wait:
+            Serial.println("Elevador en espera...");
+            Serial.print("Wating");
+            for (size_t i = 0; i < 3; i++) {
+                delay(1000);
+                Serial.print(".");
+            }
+            Serial.print("\n");
+            break;
+
+        default:
+            Serial.println("ERROR");
+            break;
+    }
 }
 
 void loop() {
+    for (int i = 0; i < 3; i++){
+        listaEstados[i];
+        if(digitalRead(Button1) == 1 || digitalRead(Button2) == 1) {
+            i ++;
+            if (i == 2){
+                i = 0;
+            }
+        }
+        else {
+            i == 0;
+        } 
+    }
 }
--- Sem8/horno/horno.ino
+++ Sem8/horno/horno.ino
@@ -1,6 +1,42 @@
+// horno de microondas.
+// dos botones para arranque de tiempos de 5, 10 y 20.
+// La puerta se bloquea durante el arranque.
+// al final se desbloquea la puerta.
+
+#define Time5 = 13
+#define Time10 = 14
+#define Time20 = 15
+#define DoorBlock = 17
 
 void setup() {
+    Serial.begin(9600);
+    pinMode(13, INPUT);
+    pinMode(14, INPUT);
+    pinMode(15, INPUT);
+    pinMode(16, INPUT);
+    pinMode(17, OUTPUT);
+}
+
+void Work(int Time) {
+    Serial.println("Bloqueando puerta.");
+    digitalWrite(17, HIGH);
+    Serial.println("Cocinando por " + Time);
+    for (int i = 0; i <= Time; i++) {
+        Serial.println(".");
+        delay(1000);
+    }
+    digitalWrite(17, LOW);
+    Serial.println("Desbloqueando puerta.");
 }
 
 void loop() {
+    if (digitalRead(13 == 1)) {
+        Work(5);
+    }
+    else if (digitalRead(14 == 1)) {
+        Work(10);
+    }
+    else if (digitalRead(15 == 1)) {
+        Work(20);
+    }
 }
